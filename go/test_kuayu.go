cae0cae4b2c44f8d22a929ac87a35cf388405f0f
任 <kealuya@126.com>
2021-11-09 09:58:24 +0800

    跨域测试，为后续门户权限做准备

---
+++ go/test_kuayu.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		allowOrigin := []string{"http://localhost:3000", ""}
+		h := request.Header
+		origin := h["Origin"]
+		fmt.Println(origin)
+		fmt.Println(request.Header)
+		for _, v := range allowOrigin {
+			if v == origin[0] {
+				writer.Header().Set("Access-Control-Allow-Origin", v)
+				break
+			}
+		}
+		writer.Write([]byte("is ok"))
+	})
+
+	http.ListenAndServe(":8000", nil)
+
+}
